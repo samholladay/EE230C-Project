673eeab5230591aa483f8bc14a29c19767ab656b
Sam Holladay <sam.holladay@berkeley.edu>
2016-12-09 00:46:22 -0800

    Need integration

--- calculate_Id_vg.m
+++ calculate_Id_vg.m
@@ -1,18 +1,20 @@
 function [Id, Vd] = calculate_Id_vg(k, Ek)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-clear all;
 kbT = 0.026;
 h_bar=(6.626e-34)/(2*pi);
 q=1.6e-19;
 w=10e-9; % How wide is the transistor?
 ss=0.01; % Step size
 Vd=0:ss:8;
+mu=0;
 
 % Recall v=(1/h_bar)dE/dk
 grad = diff(Ek)/ss;
 v = (1/h_bar).*grad;
-fermi=1./(1+exp((Ek-mu)./kbT)); % What is mu?
+length=size(Ek,1);
+Ek_cut=Ek(2:length,:);
+fermi=1./(1+exp((Ek_cut-mu)./kbT)); % What is mu?
 integrand = (1/(2*pi))*fermi*v;
 fk_vk =integral(integrand,0,1e15); % Integrate to infinity
 Id=-q.*w.*fk_vk;
@@ -20,5 +22,6 @@ Id=-q.*w.*fk_vk;
 
 plot(Vd,Id);
 xlabel('Vd');
+clear all;
 end
 
