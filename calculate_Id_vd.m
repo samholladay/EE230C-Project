88a27c44d102030e935877a87c09565790d82b34
Saavan Patel <saavs94@gmail.com>
2016-12-10 23:33:16 -0800

    Changed some stuff. Did some things. Everything is incorrect (vth = -2e22?????)

--- calculate_Id_vd.m
+++ calculate_Id_vd.m
@@ -2,13 +2,13 @@ function [Id, Vd] = calculate_Id_vg(Ek)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 kbT = 0.026;
+a = 1.42e-10; %Graphene lattice constant
 h_bar=(6.626e-34)/(2*pi);
 q=1.6e-19;
-w=10e-9; % How wide is the transistor?
-ss=0.01; % Step size
-Vd=0:ss:8;
+w=1e-6; % How wide is the transistor?
+Vd=linspace(0,0.5,500);
 mu=0;
-k_step = 1;
+k_step = pi/(a*size(Ek,1));
 
 % Recall v=(1/h_bar)dE/dk
 grad = diff(Ek)/k_step;
@@ -17,18 +17,18 @@ length=size(Ek,1);
 Ek_cut=Ek(2:length,:);
 fermi=1./(1+exp((Ek_cut-mu)./kbT)); % What is mu?
 integrand = (1/(2.*pi)).*fermi.*v;
-x=linspace(0,1e6,size(Ek_cut,1));
+x=linspace(0,1e-10,size(Ek_cut,1));
 % MUST USE X TO DEFINE LIMITS OF INTEGRATION
 fk_vk =trapz(x,integrand); % Integrate to infinity
 fk = trapz(x,fermi);
 vth = nansum(fk_vk./fk);
+disp(vth)
 %Id=-q.*w.*fk_vk;
-Vd=linspace(0,2,500);
+
 v_inj=vth.*(1-exp(-Vd./kbT))./(1+exp(-Vd./kbT));
 Id=-q.*w.*v_inj;
 
 plot(Vd,Id);
 %xlabel('Vd');
-clear all;
 end
 
